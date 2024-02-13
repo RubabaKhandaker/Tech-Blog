0ae418ba401953be8ce514a1482a0f34fb1d8d4e
RubabaKhandaker <140378374+RubabaKhandaker@users.noreply.github.com>
2024-02-13 17:00:10 +1100

    Added code to config, db, and models files

---
+++ models/Comment.js
@@ -0,0 +1,66 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+
+  {
+
+    id: {
+
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+
+    },
+
+    user_id: {
+
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+
+          model: 'user',
+          key: 'id',
+
+        },
+
+      },
+
+    text: {
+
+      type: DataTypes.TEXT,
+      allowNull: false,
+
+    },
+
+    post_id: {
+
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+
+        model: 'post',
+        key: 'id',
+        
+      },
+
+    },
+
+  },
+
+  {
+
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+
+  }
+
+);
+
+module.exports = Comment;
